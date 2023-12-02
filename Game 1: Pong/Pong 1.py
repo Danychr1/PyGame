4cad374d6d9e53201588ffac60acf5f071ddb497
Dany Christel <151681252+Danychr1@users.noreply.github.com>
2023-12-01 18:49:47 -0800

    Add files via upload

---
+++ Game 1: Pong/Pong 1.py
@@ -0,0 +1,17 @@
+# pong 1
+# Code with Mr Wagner
+# Your name
+
+import time
+
+import pygame
+
+pygame.init()
+
+# create a window
+pygame.display.set_caption("Pong")
+screen = pygame.display.set_mode((800, 600))
+
+pygame.event.wait()
+time.sleep(6)
+pygame.quit()
